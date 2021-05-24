83cf77650cf6d81e9d3a3f608697cbcfc772c0c8
n-desantis92 <dsnwebdev@gmail.com>
2021-05-24 17:09:43 +0200

    creato collegamento tra Post e Comment

--- app/Comment.php
+++ app/Comment.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Comment extends Model
 {
-    //
+    protected $guarded = [];
+    public function post()
+    {
+        return $this->belongsTo('App\Post');
+    }
 }
